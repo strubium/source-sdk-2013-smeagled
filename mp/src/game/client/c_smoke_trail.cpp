d188d2ed00c1bba0f591ddc4b1af4b0e4be0da64
strubium <113206902+strubium@users.noreply.github.com>
2024-02-28 09:31:12 -0600

    Update c_smoke_trail.cpp

--- mp/src/game/client/c_smoke_trail.cpp
+++ mp/src/game/client/c_smoke_trail.cpp
@@ -946,7 +946,10 @@ void C_SporeExplosion::OnDataChanged( DataUpdateType_t updateType )
 		m_teParticleSpawn.Init( m_flSpawnRate );
 	}
 
-	m_pSporeEffect->SetDontRemove( m_bDontRemove );
+	if( m_pSporeEffect.IsValid() )
+	{
+		m_pSporeEffect->SetDontRemove( m_bDontRemove );
+	}
 }
 
 //-----------------------------------------------------------------------------
@@ -977,6 +980,11 @@ void C_SporeExplosion::Start( CParticleMgr *pParticleMgr, IPrototypeArgAccess *p
 //-----------------------------------------------------------------------------
 void C_SporeExplosion::AddParticles( void )
 {
+	if( !m_pSporeEffect )
+	{
+		return;
+	}
+	
 	//Spores
 	Vector	offset;
 	Vector	dir;
