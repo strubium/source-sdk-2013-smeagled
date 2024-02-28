5902b467ca6c95387266ee8f6232304ba7cff7c4
strubium <113206902+strubium@users.noreply.github.com>
2024-02-28 09:31:43 -0600

    Update ssemath.h

--- mp/src/public/mathlib/ssemath.h
+++ mp/src/public/mathlib/ssemath.h
@@ -931,10 +931,10 @@ FORCEINLINE fltx4 UnsignedIntConvertToFltSIMD( const u32x4 &vSrcA )
 {
 	Assert(0);			/* pc has no such operation */
 	fltx4 retval;
-	SubFloat( retval, 0 ) = ( (float) SubInt( retval, 0 ) );
-	SubFloat( retval, 1 ) = ( (float) SubInt( retval, 1 ) );
-	SubFloat( retval, 2 ) = ( (float) SubInt( retval, 2 ) );
-	SubFloat( retval, 3 ) = ( (float) SubInt( retval, 3 ) );
+	SubFloat( retval, 0 ) = ( (float) (reinterpret_cast<const uint32 *>(&vSrcA)[0]));
+	SubFloat( retval, 1 ) = ( (float) (reinterpret_cast<const uint32 *>(&vSrcA)[1]));
+	SubFloat( retval, 2 ) = ( (float) (reinterpret_cast<const uint32 *>(&vSrcA)[2]));
+	SubFloat( retval, 3 ) = ( (float) (reinterpret_cast<const uint32 *>(&vSrcA)[3]));
 	return retval;
 }
 
